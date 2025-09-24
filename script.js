792616f5219aab3265b4747778bbbf3371e4d7f9
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-09-25 01:02:05 +0200

    add dark mode toggle

--- script.js
+++ script.js
@@ -91,3 +91,18 @@ function initTaskBoard() {
 
 // Wait until DOM is fully loaded
 document.addEventListener("DOMContentLoaded", initTaskBoard);
+
+
+
+// Dark mode toggle
+const darkModeToggle = document.getElementById("theme-toggle-button");
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  const logo= document.getElementById("logo");
+  if (document.body.classList.contains("dark-mode")) {
+    logo.src = "./assets/logo-dark.svg";
+  }
+  else {
+    logo.src = "./assets/logo-light.svg";
+  }
+});  // swap logo image based on mode
